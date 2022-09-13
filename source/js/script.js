fdc9753b12b2a287b61c4d25d9f5e4b379ada407
Aleksei <im0602060@gmail.com>
2022-09-13 17:59:50 +0900

    переделал выпадающее меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,9 @@
-let burgerButton = document.getElementById("burger-button");
-let closeButton = document.getElementById("close-button");
-let element = document.getElementById("navigation");
+let buttonToggle = document.getElementById("button-toggle");
+let navigation = document.getElementById("navigation");
 
-burgerButton.onclick = showMobileMenu;
+buttonToggle.onclick = showMobileMenu;
 
 function showMobileMenu() {
-    element.classList.remove("navigation--hidden");
-    closeButton.classList.remove("button-hidden");
-}
-
-closeButton.onclick = closeMobileMenu;
-
-function closeMobileMenu () {
-    element.classList.add("navigation--hidden");
-    closeButton.classList.add("button-hidden");
+    buttonToggle.classList.toggle("button-toggle--close");
+    navigation.classList.toggle("navigation-hidden");
 }
