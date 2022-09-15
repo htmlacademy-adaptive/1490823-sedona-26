8218a1a95472f29360b637db1369ce993bca7d10
Keks <keks@htmlacademy.ru>
2022-09-15 20:31:13 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/script.js
@@ -0,0 +1,11 @@
+let buttonToggle = document.getElementById("button-toggle");
+let navigation = document.getElementById("navigation");
+
+console.log(navigation);
+
+buttonToggle.onclick = showMobileMenu;
+
+function showMobileMenu() {
+    buttonToggle.classList.toggle("button-toggle--close");
+    navigation.classList.toggle("navigation--hidden");
+}
