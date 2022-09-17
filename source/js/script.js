e2b2da51021504f5f2c40b5f4253771f8322481f
Aleksei <im0602060@gmail.com>
2022-09-17 17:36:34 +0900

    сделал нижний лого меняющим цвет вместе с подписью

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 const buttonToggle = document.querySelector(".button-toggle");
 const navigation = document.querySelector(".navigation");
 
-buttonToggle.addEventListener("click", ()=>
-  buttonToggle.classList.toggle("button-toggle--close"),
-  navigation.classList.toggle("navigation--hidden")
-)
+buttonToggle.addEventListener("click", showMobileMenu);
+
+function showMobileMenu() {
+    buttonToggle.classList.toggle("button-toggle--close");
+    navigation.classList.toggle("navigation--hidden");
+}
