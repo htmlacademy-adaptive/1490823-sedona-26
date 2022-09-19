9bb2748edd1545a5de904833c44b2b0646d69d2d
Keks <keks@htmlacademy.ru>
2022-09-19 21:34:44 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/script.js
+++ 12/js/script.js
@@ -1,9 +1,1 @@
-const buttonToggle = document.querySelector(".button-toggle");
-const navigation = document.querySelector(".navigation");
-
-buttonToggle.addEventListener("click", showMobileMenu);
-
-function showMobileMenu() {
-    buttonToggle.classList.toggle("button-toggle--close");
-    navigation.classList.toggle("navigation--hidden");
-}
+const buttonToggle=document.querySelector(".button-toggle"),navigation=document.querySelector(".navigation");function showMobileMenu(){buttonToggle.classList.toggle("button-toggle--close"),navigation.classList.toggle("navigation--hidden")}buttonToggle.addEventListener("click",showMobileMenu);
