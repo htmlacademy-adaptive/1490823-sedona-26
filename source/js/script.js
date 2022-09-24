d9a161f4f73d19f95d24bc9367d7b482d98d379f
Aleksei <im0602060@gmail.com>
2022-09-24 20:14:04 +0900

    исправил 9 ошибок проверяющего, ещё есть несколько недочетов.

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,11 @@
 const buttonToggle = document.querySelector(".button-toggle");
 const navigation = document.querySelector(".navigation");
 
+navigation.classList.add("navigation--hidden");
+
 buttonToggle.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-    buttonToggle.classList.toggle("button-toggle--close");
-    navigation.classList.toggle("navigation--hidden");
+  buttonToggle.classList.toggle("button-toggle--close");
+  navigation.classList.toggle("navigation--hidden");
 }
